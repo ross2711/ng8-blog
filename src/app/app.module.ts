3a80b60413cd664e8cfea13590f7d3a9fcac5880
Ross Fleming <rossfleming2711@gmail.com>
2019-06-24 16:25:19 +0200

    page is responsive. side nav added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,9 +8,16 @@ import { MaterialModule } from "./material.module";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { HomeComponent } from "./home/home.component";
+import { HeaderComponent } from "./navigation/header/header.component";
+import { SidenavListComponent } from "./navigation/sidenav-list/sidenav-list.component";
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent],
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    SidenavListComponent
+  ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
